4a595b37128cf322f423f42b9f1f4592e5761bd2
Thummanoon hwancaheam <42062759+artborza@users.noreply.github.com>
2021-04-20 21:21:59 +0700

    4/20/20201 9:21 PM

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../models/product';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+  products:Product[]
+  constructor() { 
+    this.products =[];
+    this.products.push({
+      name:'ส้มโอ',
+      price:990
+    })
+    this.products.push({
+      name:'แตงโม',
+      price:1000
+    })
+    this.products.push({
+      name:'มะพร้าว',
+      price:50
+    })
+  }
+
+  ngOnInit(): void {
+  }
+
+}
